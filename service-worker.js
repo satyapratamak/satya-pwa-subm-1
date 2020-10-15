693bab9c7c9763df4cc36f8af4b38a10a9edaebc
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2020-10-15 20:55:46 +0700

    COMMIT : Default PWA

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "firstpwa-v1";
+
+var urlsToCache = [
+    "/",
+    "/icon.png",
+    "/nav.html",
+    "/index.html",
+    "/manifest.json",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/contact.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "/js/nav.js"
+  ]; // list of page that content Assets, JS, and styles
+
+
+// Install static Cache to ServiceWorker
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+});
+
+
+// Using aset from cache
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function(response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+ 
+        console.log(
+          "ServiceWorker: Memuat aset dari server: ",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
+
+// Deleting older cache
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
